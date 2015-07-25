14cab2ab361bc6b86b255395cc573890bbd5ad8b
ilya n <ilya.nikokoshev@gmail.com>
2015-07-25 12:00:59 +0300

    strings

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let x:NSString = "😡"
+x.length
+
+let y = "😡"
+y.characters.count
+
+
+let eAcute:NSString = "\u{E9}"                         // é
+eAcute.length
+
+let combinedEAcute:NSString = "\u{65}\u{301}"
+combinedEAcute.length
+
+let url = NSURL(string: "https://en.wikipedia.org/wiki/Elliott_Fitch_Shepard")
+
+url?.scheme
+url?.path
+
+/*
+
+{
+        name: "Ученый кот", 
+        badge: ["iOS", "Swift"]
+}
+
+*/
+
+//let data = NSData(contentsOfURL: url!)
+//
+//NSString(data: data!, encoding: NSUTF8StringEncoding)
+
