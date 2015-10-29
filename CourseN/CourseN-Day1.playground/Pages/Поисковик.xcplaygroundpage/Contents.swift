55559d59777a257fb919016f5d854e57851cb56f
ilya n <ilya.nikokoshev@gmail.com>
2015-10-29 13:31:03 +0300

    Search

--- CourseN/CourseN-Day1.playground/Pages/Поисковик.xcplaygroundpage/Contents.swift
+++ CourseN/CourseN-Day1.playground/Pages/Поисковик.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
 
 
 
-let names = ["Вася", "Илья", "Петя Ш", "Катя" ]
+let names = ["Вася", "Илья Н", "Петя Ш", "Катя", "Илья Е", "Наталья", "Женя", "Доротья", "Марфия", "Япоша" ]
 
 func ЭтоВ(name: String) -> Bool {
     return name.characters.first == "В"
@@ -19,6 +19,24 @@ func ЭтоШ(name: String) -> Bool {
 }
 
 names.filter(ЭтоВ)
+
+
+func КонецЯ(name: String) -> Bool {
+    return name.characters.last == "я"
+}
+
+names.filter(КонецЯ)
+
+
+func Конец(конец: Character) -> (String -> Bool) {
+    
+    func f(name:String) -> Bool {
+        return name.characters.last == конец
+    }
+    
+    return f
+}
+
 
 //: Это пример типа функций
 
@@ -46,12 +64,48 @@ func ЭтоСоСравнением(test:Character? -> Bool) -> (String -> Bool)
 }
 
 //: Короткая форма описания функции: имя аргумента можно опустить, как и круглые скобки вокруг фигурных.
+func тестер(char:Character?) -> Bool {
+    return char == "В" || char == "П"
+}
+
+names.filter(ЭтоСоСравнением(тестер))
 
 names.filter(ЭтоСоСравнением({ char in
     char == "В" || char == "П"
 }))
 
-//: Написать поисковик, который изет по всем текстам какую-то букву
+//: Написать поисковик, который ищет по всем текстам какую-то букву
+
+names
+
+func search_factory(чемто: Character) -> String -> [(Int, Character, String)] {
+    
+    return { name in
+    
+        return name
+                .lowercaseString
+                .characters
+                .enumerate()
+                
+                .map {
+                    ($0, $1, name)
+                }
+                
+                .filter { (index, char, string) in
+                    char == чемто
+        }
+    }
+
+}
+
+names
+    .map(search_factory("ф"))
+    .reduce([], combine:+)
+    .map { index, char, name in
+        "Привет, \( name )!"
+    }
+    .joinWithSeparator("\n")
+
 
 names.filter(Это("В"))
 
