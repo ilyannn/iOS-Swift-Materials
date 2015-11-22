c2c4f99c8e9f9d21c8a0db4101b60979d74db630
ilya n <ilya.nikokoshev@gmail.com>
2015-11-22 17:35:26 +0300

    Code improvements

--- Course3/Course3-Day2/Course3-Day2/ViewController.swift
+++ Course3/Course3-Day2/Course3-Day2/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let titleColor = UIColor(red: 0, green: 1, blue: 0.1, alpha: 1)
+    var animals = ["Кошка", "Собака"]
+    
+    @IBOutlet weak var screenTitle: UILabel!
+    @IBOutlet weak var screenSubtitle: UILabel!
+    @IBOutlet weak var catPicture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        screenTitle.text = "Заголовок экрана"
+        screenTitle.textColor = titleColor
+        
+        screenSubtitle.text = "Подзаголовок!"
+        screenSubtitle.textColor = UIColor(hue: 0.3, saturation: 1, brightness: 0.1, alpha: 1)
+        
+        screenTitle.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.075)
     }
 
     override func didReceiveMemoryWarning() {
