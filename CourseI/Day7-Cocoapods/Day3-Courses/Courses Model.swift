db7e5b40fd5d6c1b96e84c36c973fb523337d045
ilya n <ilya.nikokoshev@gmail.com>
2016-01-10 16:05:58 +0300

    Separate v3

--- CourseI/Day7-Cocoapods/Day3-Courses/Courses Model.swift
+++ CourseI/Day7-Cocoapods/Day3-Courses/Courses Model.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-private let json = NSURL(string: "https://raw.githubusercontent.com/ilyannn/iOS-Swift-Materials/master/CourseI/courses-v2.json")
+private let json = NSURL(string: "https://raw.githubusercontent.com/ilyannn/iOS-Swift-Materials/master/CourseI/courses-v3.json")
 
 private func jsonFrom(maybe_url: NSURL?) -> AnyObject {
     
