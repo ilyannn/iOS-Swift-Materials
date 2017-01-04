6d4be6a3fa92a05dbb456f1fa6b58b7046c61a43
ilya n <ilya.nikokoshev@gmail.com>
2017-01-04 19:21:40 +0300

    Visual effects

--- Course17A/Day2/Day2/ViewController.swift
+++ Course17A/Day2/Day2/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var seasonPhoto: UIImageView!
+    @IBOutlet weak var charactersButton: UIButton!
+    @IBOutlet weak var episodeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        charactersButton.backgroundColor = UIColor.red
+        charactersButton.layer.cornerRadius = 20
+        
+//        seasonPhoto.transform = CGAffineTransform(rotationAngle: 3.14/12)
+     
+        UIView.animate(withDuration: 5) {
+            self.seasonPhoto.layer.transform = CATransform3DMakeRotation(3.14/4, 1, -1, 1)
+        }
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       options: [.autoreverse, .repeat, .allowUserInteraction],
+                       animations:
+        {
+            self.charactersButton.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+            
+        }, completion: {_ in })
+        
+        seasonPhoto.layer.cornerRadius = 20
+        seasonPhoto.clipsToBounds = true
+        
+        episodeLabel.text = episodeLabel.text?.uppercased()
+        episodeLabel.layer.shadowColor = UIColor.black.cgColor
+        episodeLabel.layer.shadowOpacity = 1
+        episodeLabel.layer.shadowOffset = CGSize(width: 3, height: 3)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Course17A/var.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str.append("!")
+str
+
+var year = 2016
+year = 2017
+
+var objects = ["Obj-C"]
+objects.append("Swift")
+objects
+
+
