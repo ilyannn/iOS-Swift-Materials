af0d988c9b348cc14ba041c39d26f7396a0825cc
ilya n <ilya.nikokoshev@gmail.com>
2017-01-04 15:55:14 +0300

    Начало работы

---
+++ Course17A/Day2/Day2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day2
+//
+//  Created by iOS Swift Course on 1/4/17.
+//  Copyright © 2017 iOS Swift Course. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
